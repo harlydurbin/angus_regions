5b099daae75dd422d86003a965d4eeae55570ced
Harly Durbin <hjdzpd@mail.missouri.edu>
2021-03-16 13:41:03 -0500

    update solutions_boxswarm.R

--- source_functions/solutions_boxswarm.R
+++ source_functions/solutions_boxswarm.R
@@ -5,15 +5,15 @@ solutions_boxswarm <- function(df, effect_var, y_title){
   effect_var <- rlang::enquo(effect_var)
   
   df %>%
-    dplyr::mutate(region = forcats::as_factor(region)) %>%
+    arrange(region) %>% 
+    dplyr::mutate(region2 = forcats::as_factor(region),
+                  region = forcats::as_factor(region)) %>%
     dplyr::rename(value := !!effect_var) %>% 
     ggplot2::ggplot(aes(x = forcats::fct_reorder(region,
                                                  value,
                                                  median),
                         y = value)) +
-    ggforce::geom_sina(aes(color = forcats::fct_reorder(region,
-                                                        value,
-                                                        median)),
+    ggforce::geom_sina(aes(color = region2),
                        alpha = 0.25,
                        kernel = "rectangular") +
     ggplot2::scale_color_manual(values = c("1" = "tomato2",
@@ -30,9 +30,7 @@ solutions_boxswarm <- function(df, effect_var, y_title){
                                            "7" = "Forested Mountains",
                                            "8" = "Fescue Belt",
                                            "9" = "Upper Midwest & Northeast")) +
-    ggplot2::geom_boxplot(aes(fill = forcats::fct_reorder(region,
-                                                          value,
-                                                          median)),
+    ggplot2::geom_boxplot(aes(fill = region2),
                           alpha = 0.1,
                           show.legend = FALSE) +
     ggplot2::scale_fill_manual(values = c("1" = "tomato2",
@@ -42,19 +40,25 @@ solutions_boxswarm <- function(df, effect_var, y_title){
                                           "7" = "deeppink3",
                                           "8" = "gray17",
                                           "9" = "slateblue2")) +  
-    ggplot2::guides(colour = ggplot2::guide_legend(override.aes = list(alpha = 1))) +
+    ggplot2::guides(colour = ggplot2::guide_legend(override.aes = list(alpha = 1),
+                                                   nrow = 3,
+                                                   byrow = TRUE)) +
     ggplot2::theme_classic() +
     ggplot2::theme(axis.title = ggplot2::element_text(size = 16),
+                   axis.title.x = element_blank(),
                    axis.title.y = ggplot2::element_text(margin = margin(t = 0,
                                                                         r = 13,
                                                                         b = 0,
                                                                         l = 0)),
-                   axis.title.x = ggplot2::element_text(margin = margin(t = 13,
-                                                                        r = 0,
-                                                                        b = 0, l = 0)),
                    axis.text = ggplot2::element_text(size = 14),
-                   legend.text = ggplot2::element_text(size = 14)) +
-    ggplot2::labs(x = "Region",
+                   axis.text.x = element_blank(),
+                   axis.ticks.x = element_blank(),
+                   legend.text = ggplot2::element_text(size = 14),
+                   legend.position = "bottom",
+                   legend.margin = margin(),
+                   legend.box.just = "left",
+                   legend.justification = "left") +
+    ggplot2::labs(x = NULL,
                   y = y_title,
                   title = NULL,
                   color = NULL)
